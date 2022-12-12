8a549665b9e4cb3af3ad38299be2caf06e5aff63
srahalh-sh <100698828+srahalh-sh@users.noreply.github.com>
2022-12-12 15:33:22 +0100

    fix: :bug: header name to uppercase

--- src/shop/routers/AppRouter.js
+++ src/shop/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Header from '../components/Header/Header'
+import Header from '../components/Header/Header';
 import { ProductDetailsPage } from '../pages/ProductDetailsPage/ProductDetailsPage';
 import { ProductListPage } from '../pages/ProductListPage';
 
